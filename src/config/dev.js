159b49e6b9804ce772bd919ef0bfdc9983c564b0
Nguyễn Trần Anh Duy <84486806+Anhduy-git@users.noreply.github.com>
2022-10-26 16:40:06 +0700

    update config file

--- src/config/dev.js
+++ src/config/dev.js
@@ -1,7 +1,7 @@
 module.exports = {
 	port: process.env.PORT,
 	mongodb_url: process.env.MONGODB_URL_DEV,
-	rabbitmq_url: process.env.RABBITMQ_URL_PROD,
+	rabbitmq_url: process.env.RABBITMQ_URL_DEV,
 	jwt_secret: process.env.JWT_SECRET_DEV,
 	cloudinary_name: process.env.CLOUDINARY_NAME_DEV,
 	cloudinary_api_key: process.env.CLOUDINARY_API_KEY_DEV,
